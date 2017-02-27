961fdc0cda2d0539f2e6854247b5ba11c4def7b6
Fuad Balashov <Fuad.Balashov@appian.com>
2017-02-26 19:51:49 -0500

    SP-00004: Add index js

---
+++ index.js
@@ -0,0 +1,6 @@
+// This is a mock app for a demo on software engineering
+
+// most files in this project have nothing in them with the exception of our
+// entry point for the spin activity.
+
+// Look at files for the api's they provide you
